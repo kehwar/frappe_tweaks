88aa0b2ea40b334a541298662b15b978906c12fe
ARIADNE-PC <erickkwr@gmail.com>
2025-01-16 16:40:02 +0000

    feat: add regex utils

--- tweaks/custom/utils/safe_exec.py
+++ tweaks/custom/utils/safe_exec.py
@@ -4,6 +4,7 @@ import traceback
 import frappe
 from frappe import _
 from frappe.utils import safe_exec
+from frappe.utils.safe_exec import NamespaceDict
 
 
 def log_traceback():
@@ -31,6 +32,22 @@ def admin_sql(query, *args, **kwargs):
     return frappe.db.sql(query, *args, **kwargs)
 
 
+def get_re_module():
+    return NamespaceDict(
+        match=re.match,
+        search=re.search,
+        findall=re.findall,
+        finditer=re.finditer,
+        sub=re.sub,
+        subn=re.subn,
+        split=re.split,
+        fullmatch=re.fullmatch,
+        compile=re.compile,
+        escape=re.escape,
+        purge=re.purge,
+    )
+
+
 def get_safe_globals(get_safe_globals):
 
     def _get_safe_globals():
@@ -41,7 +58,7 @@ def get_safe_globals(get_safe_globals):
             "frappe.db.unsafe_sql": admin_sql,
             "frappe.has_permission": frappe.has_permission,
             "has_permission": frappe.has_permission,
-            "re": re,
+            "re": get_re_module(),
             "traceback.format_stack": traceback.format_stack,
         }
 
@@ -62,4 +79,4 @@ def get_safe_globals(get_safe_globals):
 def apply_safe_exec_patches():
     safe_exec.get_safe_globals = get_safe_globals(safe_exec.get_safe_globals)
     safe_exec.WHITELISTED_SAFE_EVAL_GLOBALS["len"] = len
-    safe_exec.WHITELISTED_SAFE_EVAL_GLOBALS["re"] = re
+    safe_exec.WHITELISTED_SAFE_EVAL_GLOBALS["re"] = get_re_module()
