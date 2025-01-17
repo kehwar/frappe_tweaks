59f150dd4c6ef67e021aecc79ad0535574c000f8
ARIADNE-PC <erickkwr@gmail.com>
2025-01-17 20:07:24 +0000

    feat: skip with flags

--- tweaks/tweaks/doctype/server_performance_log/server_performance_log_task.py
+++ tweaks/tweaks/doctype/server_performance_log/server_performance_log_task.py
@@ -1,9 +1,20 @@
+import frappe
 from frappe import get_site_path, new_doc
 from frappe.utils import now
 from psutil import cpu_percent, disk_usage, virtual_memory
 
 
 def execute():
+
+    if (
+        frappe.flags.in_patch
+        or frappe.flags.in_install
+        or frappe.flags.in_migrate
+        or frappe.flags.in_import
+        or frappe.flags.in_setup_wizard
+    ):
+        return
+
     perf_log = new_doc("Server Performance Log")
     perf_log.cpu_utilisation = cpu_percent()
     perf_log.ram_utilisation = virtual_memory().percent
