dc0beb21255493875575a00fc5e8a13f557dce40
Kehwar <erickkwr@gmail.com>
2025-04-11 21:52:33 +0000

    feat: add authentication patch

--- tweaks/custom/patches.py
+++ tweaks/custom/patches.py
@@ -7,10 +7,12 @@ from tweaks.custom.doctype.workflow import apply_workflow_patches
 from tweaks.custom.utils.db_query import apply_db_query_patches
 from tweaks.custom.utils.fixtures import apply_fixtures_patches
 from tweaks.custom.utils.safe_exec import apply_safe_exec_patches
+from tweaks.custom.utils.authentication import apply_authentication_patches
 
 
 def apply_patches():
 
+    apply_authentication_patches()
     apply_client_script_patches()
     apply_db_query_patches()
     apply_document_patches()
---
+++ tweaks/custom/utils/authentication.py
@@ -0,0 +1,35 @@
+import frappe
+from frappe import auth
+from frappe.core.doctype.user.user import User
+
+def decorate_validate_api_key_secret(validate_api_key_secret):
+    """Decorator for `auth.validate_api_key_secret`"""
+
+    def _validate_api_key_secret(api_key, api_secret, frappe_authorization_source=None):
+
+        # Try to validate API key and secret as user credentials first.
+        if validate_api_token_as_user_password(api_key, api_secret):
+            return
+        
+        # If that fails, fall back to the original `validate_api_key_secret` function.
+        return validate_api_key_secret(api_key, api_secret, frappe_authorization_source)
+
+    return _validate_api_key_secret
+
+def validate_api_token_as_user_password(user, password):
+    """Validate API token as user credentials."""
+
+    credentials = User.find_by_credentials(user_name=user, password=password)
+
+    if credentials and credentials.enabled and credentials.is_authenticated:
+        # Store `form_dict`, because `set_user` clears it.
+        form_dict = frappe.local.form_dict
+        # Set the user as authenticated.
+        frappe.set_user(user)
+        # Restore `form_dict`.
+        frappe.local.form_dict = form_dict
+        return True
+
+def apply_authentication_patches():
+    """Apply authentication patches."""
+    auth.validate_api_key_secret = decorate_validate_api_key_secret(auth.validate_api_key_secret)
