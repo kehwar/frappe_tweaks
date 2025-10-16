fe61d79a8a67693202a5d30ca01322b94e103e87
kehwar <erickkwr@gmail.com>
2025-10-16 20:23:09 +0000

    chore: remove deprecated patch

--- tweaks/custom/patches.py
+++ tweaks/custom/patches.py
@@ -4,10 +4,9 @@ from tweaks.custom.doctype.server_script_customization import (
     apply_server_script_patches,
 )
 from tweaks.custom.doctype.workflow import apply_workflow_patches
-from tweaks.custom.utils.db_query import apply_db_query_patches
-from tweaks.custom.utils.fixtures import apply_fixtures_patches
-from tweaks.custom.utils.safe_exec import apply_safe_exec_patches
 from tweaks.custom.utils.authentication import apply_authentication_patches
+from tweaks.custom.utils.db_query import apply_db_query_patches
+from tweaks.custom.utils.safe_exec import apply_safe_exec_patches
 
 
 def apply_patches():
@@ -16,7 +15,6 @@ def apply_patches():
     apply_client_script_patches()
     apply_db_query_patches()
     apply_document_patches()
-    apply_fixtures_patches()
     apply_safe_exec_patches()
     apply_server_script_patches()
     apply_workflow_patches()
