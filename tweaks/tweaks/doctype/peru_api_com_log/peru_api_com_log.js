9c4c1eea36607b686ae7fbcfd99eeff0c5defda8
kehwar <erickkwr@gmail.com>
2025-10-16 14:47:23 +0000

    feat: remove button

--- tweaks/tweaks/doctype/peru_api_com_log/peru_api_com_log.js
+++ tweaks/tweaks/doctype/peru_api_com_log/peru_api_com_log.js
@@ -1,10 +1,10 @@
 // Copyright (c) 2025, Erick W.R. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("PERU API COM Log", {
-	refresh(frm) {
-        frm.add_custom_button(__('Settings'), function() {
-            frappe.set_route("List", "PERU API COM");
-        });
-	},
-});
+// frappe.ui.form.on("PERU API COM Log", {
+// 	refresh(frm) {
+//         frm.add_custom_button(__('Settings'), function() {
+//             frappe.set_route("List", "PERU API COM");
+//         });
+// 	},
+// });
