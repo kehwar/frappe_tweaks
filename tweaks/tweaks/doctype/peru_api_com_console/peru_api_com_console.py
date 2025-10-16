4e6d8ab0be4d2ba51d16b9909bacdc2c1450db0f
kehwar <erickkwr@gmail.com>
2025-10-16 16:34:06 +0000

    feat: allow ignore cache on console

--- tweaks/tweaks/doctype/peru_api_com_console/peru_api_com_console.py
+++ tweaks/tweaks/doctype/peru_api_com_console/peru_api_com_console.py
@@ -18,17 +18,18 @@ class PERUAPICOMConsole(Document):
 def search(doc):
 
     doc = frappe.get_doc(json.loads(doc))
+    cache = not doc.ignore_cache
 
     doc.data = None
     doc.error = None
 
     try:
         if doc.search == "RUC" and doc.search_ruc:
-            doc.data = get_ruc(doc.search_ruc.strip())
+            doc.data = get_ruc(doc.search_ruc.strip(), cache=cache)
         elif doc.search == "DNI" and doc.search_dni:
-            doc.data = get_dni(doc.search_dni.strip())
+            doc.data = get_dni(doc.search_dni.strip(), cache=cache)
         elif doc.search == "TC":
-            doc.data = get_tc(doc.search_tc)
+            doc.data = get_tc(doc.search_tc, cache=cache)
         if doc.data:
             doc.data = json.dumps(doc.data, indent=4, ensure_ascii=True)
     except Exception:
