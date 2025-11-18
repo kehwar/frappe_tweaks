0260536fedaec6a87bfb903f1bf4eda3c6eb3283
kehwar <erickkwr@gmail.com>
2025-11-18 22:25:28 +0000

    feat: update help text in AC Rule to clarify filter usage for users and resources

--- tweaks/tweaks/doctype/ac_rule/ac_rule.js
+++ tweaks/tweaks/doctype/ac_rule/ac_rule.js
@@ -8,15 +8,19 @@ frappe.ui.form.on("AC Rule", {
     setup_help(frm) {
         frm.get_field('principals_html').html(`
             <p class="help-box small text-muted">
-            This <code>rule</code> will apply to <code>users</code> <b>matching any</b> <code>principal</code> listed and <b>not matching any</b> <code>exception</code> listed
+            This <code>rule</code> will apply to <code>users</code> <b>matching any</b> <code>filter</code> listed and <b>not matching any</b> <code>exception</code> listed
             <br><i>Example: (M1 OR M2) AND !(E1 OR E2)</i>
             </p>`
         );
         frm.get_field('resources_html').html(`
             <p class="help-box small text-muted">
-            This <code>rule</code> will apply to <code>records</code> <b>matching any</b> <code>resource</code> listed and <b>not matching any</b> <code>exception</code> listed
+            This <code>rule</code> will apply to <code>records</code> <b>matching any</b> <code>filter</code> listed and <b>not matching any</b> <code>exception</code> listed
             <br><i>Example: (M1 OR M2) AND !(E1 OR E2)</i>
-            </p>`
+            </p>
+            <p class="help-box small text-muted">
+            If <b>no</b> <code>filter</code> is defined, the <code>rule</code> will apply to <b>all records</b> of the specified <code>resource</code>.
+            </p>
+            `
         );
         frm.get_field('type_html').html(`
             <p class="help-box small text-muted">
