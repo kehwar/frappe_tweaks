691942d95348c55c1c642c3d4d537cbccf67ae0f
kehwar <erickkwr@gmail.com>
2026-01-11 02:16:31 +0000

    feat: add custom button for Query Filters and link AC Rule in AC Resource

--- tweaks/tweaks/doctype/ac_resource/ac_resource.js
+++ tweaks/tweaks/doctype/ac_resource/ac_resource.js
@@ -5,6 +5,23 @@ frappe.ui.form.on("AC Resource", {
     setup(frm) {
         frm.trigger('setup_help');
     },
+    refresh(frm) {
+        if (!frm.is_new()) {
+            frm.add_custom_button(__('Query Filters'), () => {
+                if (frm.doc.type === 'DocType' && frm.doc.document_type) {
+                    frappe.route_options = {
+                        'reference_doctype': frm.doc.document_type
+                    };
+                } else if (frm.doc.type === 'Report' && frm.doc.report) {
+                    frappe.route_options = {
+                        'reference_report': frm.doc.report
+                    };
+                }
+                
+                frappe.set_route('List', 'Query Filter');
+            }, "View");
+        }
+    },
     setup_help(frm) {
         frm.get_field('actions_html').html(`
             <p class="help-box small text-muted">
