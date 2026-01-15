a430b64b08d56f4c780ec58d68abb2973cd5616b
kehwar <erickkwr@gmail.com>
2026-01-15 14:44:32 +0000

    feat(ac_action): add validation for unique scrubbed action names

--- tweaks/tweaks/doctype/ac_action/ac_action.py
+++ tweaks/tweaks/doctype/ac_action/ac_action.py
@@ -2,22 +2,43 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe import _
+from frappe import _, scrub
 from frappe.model.document import Document
 
 
 class ACAction(Document):
 
     def validate(self):
+        self.validate_unique_scrubbed_name()
 
         if self.disabled and not frappe.db.exists(
             "AC Action", [["disabled", "=", 0], ["name", "!=", self.name]]
         ):
             frappe.throw(_("At least one action must be enabled."))
 
+    def validate_unique_scrubbed_name(self):
+        """Ensure no other action exists with the same scrubbed name"""
+        scrubbed_name = scrub(self.action)
+
+        # Check if any other action has the same scrubbed name
+        existing = frappe.db.get_all(
+            "AC Action",
+            filters={"name": ["!=", self.name]},
+            fields=["name", "action"],
+            pluck="action",
+        )
+
+        for existing_action in existing:
+            if scrub(existing_action) == scrubbed_name:
+                frappe.throw(
+                    _(
+                        "An action with the scrubbed name '{0}' already exists: {1}"
+                    ).format(scrubbed_name, existing_action)
+                )
+
     def clear_cache(self):
         """Clear AC rule cache"""
-        if hasattr(super(), 'clear_cache'):
+        if hasattr(super(), "clear_cache"):
             super().clear_cache()
         from tweaks.tweaks.doctype.ac_rule.ac_rule_utils import clear_ac_rule_cache
 
@@ -25,7 +46,7 @@ class ACAction(Document):
 
     def on_trash(self):
         """Clear AC rule cache when action is deleted"""
-        if hasattr(super(), 'on_trash'):
+        if hasattr(super(), "on_trash"):
             super().on_trash()
         from tweaks.tweaks.doctype.ac_rule.ac_rule_utils import clear_ac_rule_cache
 
