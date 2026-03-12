223f99005487653dfa36296df80100f415d8ee3e
kehwar <erickkwr@gmail.com>
2026-03-12 14:19:25 +0000

    refactor(async_task): remove deprecated imports and simplify module exports

--- tweaks/utils/async_task.py
+++ tweaks/utils/async_task.py
@@ -1,23 +1,3 @@
-# Copyright (c) 2026, Erick W.R. and Contributors
-# See license.txt
+from tweaks.tweaks.doctype.async_task.async_task import enqueue_async_task
 
-# Backward-compatibility shim. All logic has moved to the doctype module.
-# Import from the canonical location instead:
-#   from tweaks.tweaks.doctype.async_task.async_task import enqueue_async_task
-
-from tweaks.tweaks.doctype.async_task.async_task import (  # noqa: F401
-    FAILURE_THRESHOLD,
-    _DISPATCH_LOCK,
-    _dispatch_method,
-    _enqueue_task,
-    _run_dispatch,
-    _save_error,
-    dispatch_async_tasks,
-    enqueue_async_task,
-    enqueue_dispatch_async_tasks,
-    execute_async_task,
-    expire_stalled_tasks,
-    update_job_id,
-)
-from frappe.utils.background_jobs import enqueue  # noqa: F401
-from frappe.utils.synchronization import filelock  # noqa: F401
+__all__ = ["enqueue_async_task"]
